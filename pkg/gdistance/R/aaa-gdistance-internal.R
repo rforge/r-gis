4decf4643fa04ea50bd6779a743372a6c16819dc
jacobvanetten <jacobvanetten@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-11-04 07:23:14 +0000

    class documentation (transition)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gis@105 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gdistance/R/aaa-gdistance-internal.R
+++ pkg/gdistance/R/aaa-gdistance-internal.R
@@ -22,28 +22,6 @@ setClass(
 		}
 )
 
-setClass(Class="transition",
-		contains = "rasterchar",
-		representation = representation(
-			zerorowcol = "logical",
-			transitionmatrix = "dsCMatrix"
-		),
-		validity = function(object){
-			if(object@zerorowcol == TRUE)
-			{
-				cond <- isTRUE(all.equal(as.integer(object@ncells), object@transitionmatrix@Dim[1], object@transitionmatrix@Dim[2])) #ncells will become floating point when classes are fully integrated in raster... 
-			}
-			if (object@zerorowcol == FALSE)
-			{
-				cond1 <- isTRUE(all.equal(object@transitionmatrix@Dim[1], object@transitionmatrix@Dim[2]))
-				cond2 <- object@ncells >= object@transitionmatrix@Dim[1]
-				cond <- cond1 & cond2
-			}
-			return(cond)
-		}
-)
-
-
 setMethod ("show" , "transition", 
 		function(object) {
 			cat("class     :" , class(object), "\n")
