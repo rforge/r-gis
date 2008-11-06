84f399120a2ffdaeb75f9931a40d4e1e2f86f743
rhijmans <rhijmans@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-11-06 01:17:58 +0000

    bug in distance function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gis@114 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/raster/R/geo.distance.R
+++ pkg/raster/R/geo.distance.R
@@ -19,30 +19,42 @@ distance.euclidean.xy <- function (point1, point2) {
 		x2 <- point2[,1]
 		y2 <- point2[,2]
 	}
-	d <- sqrt((x1 - x2)^2 + (y1 - y2)^2)
-	return(d)
+	distance <- sqrt((x1 - x2)^2 + (y1 - y2)^2)
+	return(distance)
 }
 
 distance.greatcircle.xy <- function (point1, point2, r=6378137) {
-	if (length(point1) == 2) {
-		x1 <- point1[1]
-		y1 <- point1[2]
+	# no data frames beyond this point...
+	p1 <- cbind(point1[,1], point1[,2])
+	p2 <- cbind(point2[,1], point2[,2])
+	if (length(p1) < 2) { stop('point1 should have at least 2 elements') } 
+	if (length(p1) == 2) {
+		x1 <- p1[1]
+		y1 <- p1[2]
 	} else {
-		x1 <- point1[,1]
-		y1 <- point1[,2]
+		x1 <- p1[,1]
+		y1 <- p1[,2]
 	}
-	if (length(point2) == 2) {
-		x2 <- point2[1]
-		y2 <- point2[2]
+	if (length(p2) == 2) {
+		x2 <- p2[1]
+		y2 <- p2[2]
 	} else {
-		x2 <- point2[,1]
-		y2 <- point2[,2]
+		x2 <- p2[,1]
+		y2 <- p2[,2]
 	}
 	y1 <- y1 * pi / 180;
-	x1 <- y2 * pi / 180;
+	x1 <- x1 * pi / 180;
 	y2 <- y2 * pi / 180;
 	x2 <- x2 * pi / 180;
-	cosd <- sin(y1)*sin(y2) +cos(y1)*cos(y2)*cos(x1-x2);
-	d <- r * acos(cosd);
-	return(d)
+
+	cosd <- sin(y1) * sin(y2) + cos(y1) * cos(y2) * cos(x1-x2);
+	distance <- r * acos(cosd);
+
+# supposedly more precise:	
+#	x <- sqrt((cos(y2) * sin(x1-x2))^2 + (cos(y1) * sin(y2) - sin(y1) * cos(y2) * cos(x1-x2))^2)
+#	y <- sin(y1) * sin(y2) + cos(y1) * cos(y2) * cos(x1-x2)
+#	d <- r * atan2(x, y)
+
+	return(distance)
 }
+
