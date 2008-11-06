e6d97bfd35dc4a66621dadd68e0f432c171d12bf
jacobvanetten <jacobvanetten@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-11-06 02:42:13 +0000

    mistake in setAs
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gis@118 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gdistance/R/aab-gdistance-internal.R
+++ pkg/gdistance/R/aab-gdistance-internal.R
@@ -40,7 +40,7 @@ setMethod ("initialize", "transition",
 
 setAs("transition", "dsCMatrix", function(from){from@transitionmatrix})
 
-setAs("raster","transition", function(from)
+setAs("transition", "raster" function(from)
 	{
 		new("raster",
 			projection = from@projection,
