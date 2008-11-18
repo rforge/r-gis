d1f44273a170a4b0969354af59a0171d99c62a37
rhijmans <rhijmans@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-11-18 07:03:34 +0000

    leap year and day length functions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gis@136 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/climate/R/climate.dates.R
+++ pkg/climate/R/climate.dates.R
@@ -3,8 +3,32 @@ doy.from.date <- function(date) {
 	as.numeric(format(as.Date(date), "%j"))
 }
 
-
-date.from.doy <- function(doy, year) {
-	as.Date(doy, origin=paste(year, "-01-01", sep=''))
+is.leapyear <- function(year) {
+	year <- round(year)
+    if(((year %% 100 !=0) & (year %%4 ==0)) | (year %% 400==0) )
+    { return(TRUE) 
+	} else { return(FALSE) }
 }
 
+days.in.year <- function(year) {
+	year <- round(year)
+	if (is.leapyear(year)) { return(366)
+	} else { return(365) }
+}
+
+date.from.doy <- function(doy, year) {
+	year <- round(year)
+	doy <- round(doy)
+	if (doy < 1) { 
+		year <- year-1
+		doy <- days.in.year(year) + doy
+	}
+	else if (doy > days.in.year(year)) { 
+		doy <- doy - days.in.year(year)
+		year <- year+1
+	}
+	if (doy < 1) { stop('cannot understand value for doy') }
+	if (doy > days.in.year(year)) { stop('cannot understand value for doy') }
+	
+	as.Date(doy, origin=paste(year, "-01-01", sep=''))
+}
