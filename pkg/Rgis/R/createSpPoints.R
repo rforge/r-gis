e0ff00ba6d7456c51f578c8653d302c241af15e1
rhijmans <rhijmans@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-08 03:48:08 +0000

    new helper functions to crease SpPoints from dataframe or MS Access database
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gis@140 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/Rgis/R/createSpPoints.R
@@ -0,0 +1,27 @@
+
+
+db2SpPoints <- function(database, table, xfield='lat', yfield='lon', includeNULL=FALSE, projection="+proj=longlat +datum=WGS84") {
+	if  (tolower(fileExtension(database)) == 'mdb') {
+		db <- odbcConnectAccess(database)
+	} else { 
+		db <- odbcConnect(database) 
+	}
+	
+	if (includeNULL) {
+		query <- paste("SELECT * FROM",table)
+	} else {
+		query <- paste("SELECT * FROM", table, "WHERE", xfield, "IS NOT NULL AND", yfield, "IS NOT NULL")
+	}
+	data <- sqlQuery(db, query)
+	sp <- createSpPoints(data, xfield, yfield, projection=projection)
+	return(sp)
+}
+
+
+CreateSpPoints <- function(dataframe, x='LON', y='LAT', projection="+proj=longlat +datum=WGS84") {
+	coords <- cbind(dataframe[x], dataframe[y])
+	row.names(coords) <- 1:nrow(coords)
+	proj4 <- new.CRS(projection)
+	spatpoints <- SpatialPointsDataFrame(coords=coords, data=dataframe, proj4string=proj4)
+	return(spatpoints)
+}
