fb6600793f893e6eee4c0129274a88db90ec8376
jacobvanetten <jacobvanetten@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-04-01 07:50:26 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gis@208 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/georef/R/georeference.R
+++ pkg/georef/R/georeference.R
@@ -8,12 +8,12 @@ get.coordinates <- function(cy='', sp='', co='', loc='') {
 }
 
 
-get.elevation <- function(latitude, longitude) {
-	theurl <- paste("http://ws.geonames.org/srtm3?lat=", latitude, "&lng=", longitude, sep='')
-	elevation <- scan(theurl, what='character', quiet=TRUE)
-	if (elevation < -32000) { elevation <- NA }
-	return(elevation)
-}
+#get.elevation <- function(latitude, longitude) {
+#	theurl <- paste("http://ws.geonames.org/srtm3?lat=", latitude, "&lng=", longitude, sep='')
+#	elevation <- scan(theurl, what='character', quiet=TRUE)
+#	if (elevation < -32000) { elevation <- NA }
+#	return(elevation)
+#}
 
 
 
