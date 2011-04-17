8c79d1ac5c2e572b0230fa7c0d32a3e37feb8233
jperlman <jperlman@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-04-17 21:26:36 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gis@264 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/emissions/R/N2O_tier1.R
+++ pkg/emissions/R/N2O_tier1.R
@@ -1,50 +1,15 @@
 
-N2O_tier1 <- function(Fsn, Fon, Fcr, Fsom, FosCG, FosFNR, FosFNP, Fprp, EF, rice=FALSE) {
+N2O_tier1 <- function(T1_data){
 
-	#case where the user does not supply emission factors, so defaults from table_11.1 are used
-	#??For the code fragment below (lines 7-10) I was originally getting a syntax error, but when I changed it to what it is now, it ran
-	#However,  it doesn't recognize the EF.RData file when I call the function without EF (maybe something I don't yet understand abt. environments)??
+	N2O_Ninputs_non_rice <- (T1_data$amount_kg[1] + T1_data$amount_kg[2] + T1_data$amount_kg[3] + T1_data$amount_kg[4]) * T1_data$EF[1]
 	
-	 if(missing(EF)){
-		 #EF = read.table("EF.RData")
-		 
-		 EF = read.csv("table_11.1.csv")
-		 
-		 #EF = data("table_11.1.csv", package = NULL)
-		 
-		 #thisenvir = new.env()
-		 #EF <- get(data("table_11.1.csv", package = "emissions", envir = thisenvir))
-		 #EF <- data.frame(EF)
-	 }
+	N2O_Ninputs_rice <- (T1_data$amount_kg[5] + T1_data$amount_kg[6] + T1_data$amount_kg[7] + T1_data$amount_kg[8]) * T1_data$EF[5]
 	
+	N2O_organic_soils <-  (T1_data$area_ha[9] * T1_data$EF[9]) + (T1_data$area_ha[11] * T1_data$EF[10]) + (T1_data$area_ha[11] * T1_data$EF[11]) + (T1_data$area_ha[12] * T1_data$EF[12]) + (T1_data$area_ha[13] * T1_data$EF[13])
 	
-	#case where the user supplies their own table (data frame) of emission factors
-	#??The way we implement the function, we include the different "regional" emission factors that accound for both land use and climate
-	#??However, it seems like we don't account for the different climates with the Fos (area) parameters.
-	#??We just have annual areas of cropland/grassland, nutrient-poor forest (NP), and nutrient-rich forest (NR) (but then these land types don't get divided by climate)
-	#??It seems like this messes up the second equation (N2O from organic soils), because we're just multiplying the different EFs by constant areas in each land use class, right?
-	#??So we need more parameters, right? We need areas of CG (FosCG) in temperate AND tropics.  We also need to divide up the forest by temp/trop
-	#??From the equation, it seems like NP and NR only apply to temperate; all tropical forest is considered the same, so we need just one more area parameter for tropical forest, right?
-	#??Does this make sense?  I guess this would mean 3 additional input parameters.
+	N2O_urine_dung <- (T1_data$amount_kg[14] * T1_data$EF[14]) + (T1_data$amount_kg[15] * T1_data$EF[15])
 	
-	efneeded <- c('EF1', 'EF1FR', 'EF2CGtemp', 'EF2CGtrop', 'EF2FtempR', 'EF2FtempP', 'EF2Ftrop', 'EF3PRPCPP', 'EF3PRPSO')
-	for (e in efneeded) {
-		if (! e %in% colnames(EF)) {
-			stop('variable missing in EF:', e)
-		}
-	}
-	
-	N2O_Ninputs <- (Fsn + Fon + Fcr + Fsom) * EF$EF1
-	#return(N2O_Ninputs)
-	
-	N2O_organic_soils <- (FosCG * EF$EF2CGtemp) + (FosCG * EF$EF2CGtrop) + (FosFNR * EF$EF2FtempR) + (FosFNP * EF$EF2FtempP) + (FosFNP * EF$EF2Ftrop)
-	#return(N2O_organic_soils)
-
-	N2O_urine_dung <- (Fprp * EF$EF3PRPCPP) + (Fprp * EF$EF3PRPSO)
-	#return(N2O_urine_dung)
-	
-	N2O_total_direct <- N2O_Ninputs + N2O_organic_soils + N2O_urine_dung
+	N2O_total_direct <- N2O_Ninputs_non_rice + N2O_Ninputs_rice + N2O_organic_soils + N2O_urine_dung
 	return(N2O_total_direct)
 	
-}
-
+}	
