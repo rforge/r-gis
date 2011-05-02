433449756b205e854d5be01d9147079754ecad44
jperlman <jperlman@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-02 21:47:12 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gis@279 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/emissions/R/N2O_tier1.R
+++ pkg/emissions/R/N2O_tier1.R
@@ -1,23 +1,40 @@
 
-N2O_tier1 <- function(x){
-
-	 index <- 1
+.N2O_tier2 <- function(x){
+	
+	index <- 1
 	 for (EF in x$EF){
 		 if ( (EF <  x$EF_L[index]) || (EF > x$EF_H[index])){
-			 print.noquote("Warning: emission factor outside the IPCC uncertainty bounds.")
+			 print.noquote("Warning: emission factor outside uncertainty bounds.")
 		 }
 		index <- index + 1
 	 }
+		
+	
+	sum(x$amount_kg * x$EF, na.rm=TRUE) + sum(x$area_ha * x$EF, na.rm=TRUE)
+	
+}
 
-	N2O_Ninputs_non_rice <- (x$amount_kg[1] + x$amount_kg[2] + x$amount_kg[3] + x$amount_kg[4]) * x$EF[1]
 	
-	N2O_Ninputs_rice <- (x$amount_kg[5] + x$amount_kg[6] + x$amount_kg[7] + x$amount_kg[8]) * x$EF[5]
+
+N2O_tier1 <- function(v, p){
+#v is a vector, matrix, or data.frame
+#p is a data.frame or list
+	if (missing(p)) {
+		thisenvir <- new.env()
+		path <- system.file("data", package="emissions")
+		d <- paste(path, '/EF_N2Otier1.RData', sep='')
+		p <- get(load(d, thisenvir), thisenvir)
+	}
+
+	if (is.vector(v)) {
+		v <- t(as.matrix(v))
+	}
 	
-	N2O_organic_soils <-  (x$area_ha[9] * x$EF[9]) + (x$area_ha[11] * x$EF[10]) + (x$area_ha[11] * x$EF[11]) + (x$area_ha[12] * x$EF[12]) + (x$area_ha[13] * x$EF[13])
+	stopifnot(all(colnames(v) %in% names(p)))
+	varnames <- colnames(p)
+	pp <- v[varnames]
+	apply(p, 1, function(x, ...)sum(x*pp), na.rm=TRUE)
+}
+
+
 	
-	N2O_urine_dung <- (x$amount_kg[14] * x$EF[14]) + (x$amount_kg[15] * x$EF[15])
-	
-	N2O_total_direct <- N2O_Ninputs_non_rice + N2O_Ninputs_rice + N2O_organic_soils + N2O_urine_dung
-	return(N2O_total_direct)
-	
-}	
--- pkg/emissions/R/N2O_tier2.R
+++ pkg/emissions/R/N2O_tier2.R
@@ -1,39 +1,11 @@
-
-.N2O_tier2 <- function(x){
-	
-	index <- 1
-	 for (EF in x$EF){
-		 if ( (EF <  x$EF_L[index]) || (EF > x$EF_H[index])){
-			 print.noquote("Warning: emission factor outside uncertainty bounds.")
-		 }
-		index <- index + 1
-	 }
-		
-	
-	sum(x$amount_kg * x$EF, na.rm=TRUE) + sum(x$area_ha * x$EF, na.rm=TRUE)
-	
-}
-
-	
-
 N2O_tier2 <- function(v, p){
-# v is a vector, matrix, or data.frame
-# p is a data.frame or list
-	if (missing(p)) {
-		thisenvir <- new.env()
-		path <- system.file("data", package="emissions")
-		d <- paste(path, '/EF.RData', sep='')
-		p <- get(load(d, thisenvir), thisenvir)
-	}
 
 	if (is.vector(v)) {
 		v <- t(as.matrix(v))
 	}
-	stopifnot(all(colnames(v) %in% names(p)))
-	varnames <- colnames(v)
-	pp <- p[varnames]
-	apply(v, 1, function(x, ...)sum(x*pp), na.rm=TRUE)
-}
-
-
 	
+	stopifnot(all(colnames(v) %in% names(p)))
+	varnames <- colnames(p)
+	pp <- v[varnames]
+	apply(p, 1, function(x, ...)sum(x*pp), na.rm=TRUE)
+}
