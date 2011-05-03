d502fd1f4f052e7ded1f4b37211008ef36f4b667
rhijmans <rhijmans@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-03 19:06:44 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gis@281 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/emissions/R/NMMS.R
+++ pkg/emissions/R/NMMS.R
@@ -1,29 +1,24 @@
-NMMS <- function(x){
-	
-	index <- 1
-	NMMS <- 0
-	for (syst in x$MM_system){
-		NMMS <- NMMS + ( ( (x$Nt[index] * x$NEXt_kg[index] * x$MSts[index]) * (1 - x$FRAClossms_pct[index]/100) )+  ( (x$Nt[index] * x$MSts[index] * x$Nbeddingms_kg[index]) ) )
-		index <- index + 1
-	}
-	
-	return(NMMS)
 
+
+.NMMSold <- function(x){
+	NMMS <- 0
+	for (index in 1:nrow(x)){
+		NMMS <- NMMS + ( ( (x$Nt[index] * x$NEXt_kg[index] * x$MSts[index]) * (1 - x$FRAClossms_pct[index]/100) )+  ( (x$Nt[index] * x$MSts[index] * x$Nbeddingms_kg[index]) ) )
+	}
+	return(NMMS)
 }
 
 
-# NMMS <- function(v, p){
-	
-	# if (is.vector(v)) {
-		# v <- t(as.matrix(v))
-	# }
-	
-	# stopifnot(all(rownames(p) %in% rownames(v)))  
-	# varnames <- rownames(v)  
-	# pp <- p[varnames]										##problem with this line (says: "Error in `[.data.frame`(p, varnames) : undefined columns selected")
-	# apply(v, 1, function(x, ...)sum(x*pp), na.rm=TRUE)    ##need to apply formula in line 6 in this line
-# }
- 
+NMMS <- function(v, p, na.rm=FALSE){
+
+	if (is.vector(v)) {
+		v <- t(as.matrix(v))
+	}
+	stopifnot(all(colnames(v) %in% rownames(p)))
+	p <- p[colnames(v), ,drop=FALSE]
+	p <- (p$NEXt_kg * p$MSts * (1 - p$FRAClossms_pct/100) + ( p$MSts * p$Nbeddingms_kg))
+	as.vector(apply(v, 1, function(x, ...)sum(x*p), na.rm=na.rm)  )
+ }
  
  
  
