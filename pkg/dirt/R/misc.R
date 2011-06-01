fb0c347f9eea68df55ea259e647c29210d37b6eb
rhijmans <rhijmans@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-01 07:00:40 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gis@295 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/dirt/R/misc.R
+++ pkg/dirt/R/misc.R
@@ -1,26 +1,65 @@
 
+.mergeSoilRaster <- function(r, d, props, filename='', ...) {
+	stopifnot(require(raster))
+    if (is.character(r)) {
+		r <- raster(r)
+    }
+    r <- subs(r, d[ , c('mukey', props)], by = 1, which=1:length(props)+1, filename=filename, ...)
+    r
+}
+
+
+.mergeSoilPol <- function(sp, d, filename='') {
+    if (is.character(sp)) {
+		if (!(require(rgdal))) {
+			stop("To read a shapefile you need the rgdal package; please install it")  
+		}
+		fn <- basename(sp)
+        fn <- substr(fn, 1, nchar(fn)-4)
+        sp <- readOGR(dirname(sp), fn)
+	}
+    if (! inherits(sp, 'SpatialPolygonsDataFrame')) {
+        warning('not a good sp object (should be a SpatialPolygonsDataFrame), returning data.frame')
+        return(d)
+    }
+	spd <- cbind(myid=1:nrow(sp@data), sp@data)
+	i <- which(toupper(colnames(spd)) == 'MUKEY')
+    if (length(i)==0) { stop('polygon attributes do not have a MUKEY field') }
+	spd <- merge(spd, d, by.x=i[1], by.y=1, all.x=TRUE)
+	spd <- spd[order(spd[,2]), -2]
+	colnames(spd) <- .fixNames(colnames(spd))
+	sp@data <- spd
+	if (filename != '') {
+		writeOGR(sp, filename, "soil", "ESRI Shapefile")
+	} else {
+		return(sp)
+	}
+}
+
+
+
 .fixNames <- function(x) {
-               n <- gsub('^[[:space:]]+', '',  gsub('[[:space:]]+$', '', x) )
-               nn <- n
-               n <- gsub('[^[:alnum:]]', '_', n)
-               n[nchar(n) > 10] <- gsub('_', '', n[nchar(n) > 10])
-               n[n==''] <- 'field'
-               n <- gsub('^[^[:alpha:]]', 'X', n)
-               n <- substr(n, 1, 10)
+    n <- gsub('^[[:space:]]+', '',  gsub('[[:space:]]+$', '', x) )
+    nn <- n
+    n <- gsub('[^[:alnum:]]', '_', n)
+    n[nchar(n) > 10] <- gsub('_', '', n[nchar(n) > 10])
+    n[n==''] <- 'field'
+    n <- gsub('^[^[:alpha:]]', 'X', n)
+    n <- substr(n, 1, 10)
 
        # duplicate names
-               nn  = as.matrix(table(n))
-               i = which(nn > 1)
-               if (! is.null(i)) {
-                       names = rownames(nn)[i]
-                       n[n %in% names] <- substr(n[n %in% names], 1, 9)
-                       n <- make.unique(n, sep = "")
-               }
-               if (! all(x == n)) {
-                       x = rbind(x, n)
-                       rownames(x) = c('original name', 'adjusted name')
-                       print(x)
-               }
-               return(n)
+    nn  = as.matrix(table(n))
+    i = which(nn > 1)
+    if (! is.null(i)) {
+        names = rownames(nn)[i]
+        n[n %in% names] <- substr(n[n %in% names], 1, 9)
+        n <- make.unique(n, sep = "")
+    }
+    if (! all(x == n)) {
+        x = rbind(x, n)
+        rownames(x) = c('original name', 'adjusted name')
+        print(x)
+    }
+    return(n)
 }
 
--- pkg/dirt/R/soilgo.R
+++ pkg/dirt/R/soilgo.R
@@ -1,53 +1,29 @@
 
-soilGo <- function(dat, depth=10, props=c("om_r", "claytotal_h", "claytotal_l"), sp = NULL, raster=NULL, ... ) {
+
+soilGo <- function(dat, depth=10, props=c("om_r", "claytotal_h", "claytotal_l"), filename='', sp = NULL, raster=NULL, ... ) {
+
     props <- unique(props)
 	if (tolower(substr(dat, nchar(dat)-3, nchar(dat))) == '.mdb') {
 		d <- .getSoilGoAccess(dat, props)
 	} else {
 		d <- .getSoilGoTxt(dat, props)
 	}
-	ddd <- .processGo(d, depth, props)
+	
+	d <- .processGo(d, depth, props)
 	
     if (!is.null(raster)) {
-		require(raster)
-        if (is.character(raster)) {
-			raster <- raster(raster)
-        }
-        raster <- subs(raster, ddd[ , c('mukey', props)], by = 1, which=1:length(props)+1, filename=filename, ...)
-        return(raster)
+		return(.mergeSoilRaster(raster, d, props, filename))
     } else if (!is.null(sp)) {
-		.mergeSoilPol(sp, ddd)
+		#if (is.logical(sp)) {
+		#	bname <- dat
+		# check if mdb or tabular
+		# get basename 
+		# sp = basename / spatial / soil_mu_../shp
+		#}
+		return(.mergeSoilPol(sp, d, filename))
+	} else if (filename!= '') {
+		write.table(d, file=filename, row.names=FALSE, col.names=TRUE)
 	} else {
-        return(ddd)
+        return(d)
     }
 }
-
-
-
-.mergeSoilPol <- function(sp, ddd) {
-    if (is.character(sp)) {
-		if (!(require(rgdal))) { stop("To use the 'sp' option, you need the rgdal package; please install it")  }
-		fn <- basename(sp)
-        fn <- substr(fn, 1, nchar(fn)-4)
-        sp <- readOGR(dirname(sp), fn)
-	}
-    if (! inherits(sp, 'SpatialPolygonsDataFrame')) {
-        warning('not a good sp object (should be a SpatialPolygonsDataFrame), returning data.frame')
-        return(ddd)
-    }
-    if (length(i)==0) { stop('polygon attributes do not have a MUKEY field') }
-	i <- i[0]
-	spd <- cbind(1:nrow(sp@data), sp@data)
-	i <- which(toupper(colnames(spd)) == 'MUKEY')
-	spd <- merge(spd, ddd, by.x=i, by.y=1, all.x=TRUE)
-	spd <- spd[order(spd[,2]), -c(2:4)]
-	colnames(spd) <- .fixNames(colnames(spd))
-	sp@data <- spd
-
-	if (filename == "") {
-		return(sp)
-	} else {
-		writeOGR(sp, filename, 'soil', 'ESRI Shapefile')
-	}
-}
-
